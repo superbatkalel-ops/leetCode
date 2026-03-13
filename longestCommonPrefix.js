5696638c0920d75099702da178023c6f4eeae97f
superbatkalel-ops <superbatkal.el@gmail.com>
2026-03-13 12:33:42 +0500

    Add files via upload

---
+++ longestCommonPrefix.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    for(let i = 0; i < strs[0].length; i++){
+        for(let j = 1; j < strs.length; j++){
+            if(strs[j][i] !== strs[0][i]){
+                return strs[0].slice(0, i)
+            }
+        }
+    }
+    return strs[0]
+};
+strs = ["flower","flow","Aight"]
+console.log(longestCommonPrefix(strs))
